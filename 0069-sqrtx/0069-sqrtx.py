2685e7bd4c57243b5df4a93d20d5bb70756872be
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-03 11:34:09 -0300

    Time: 2113 ms (5.01%), Space: 12.2 MB (89.41%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def mySqrt(self, x):
         i = 0
-        while i <= x + 1:
-            if i ** 2 == x:
-                return i
-            elif i ** 2 > x:
-                return i - 1
+        while i <= x:
+            if i ** 2 == x: return i
+            elif i ** 2 > x: return i - 1
             i += 1
