97fb917fa5e0d50ee583d3fe89dfb8c97801b2cd
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-06 14:47:39 -0300

    Time: 191 ms (5.15%), Space: 13.1 MB (98.9%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        seen = ["_"]
+        i, size_control = 0, 0
+        while i < len(nums) - size_control:
+            if nums[i] not in seen: seen.append(nums[i])
+            elif nums[i] in seen:
+                if nums[i] == "_":
+                    continue
+                else:
+                    nums.pop(i)
+                    i -= 1
+                    size_control += 1
+                    nums.append("_")
+            i += 1
+
+        return len(seen) - 1
