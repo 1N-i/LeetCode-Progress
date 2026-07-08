53212ff55f344ac26393d764484e4d9e8f58c505
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-08 10:24:25 -0300

    Time: 0 ms (100%), Space: 12.4 MB (16.9%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -3,6 +3,6 @@ class Solution(object):
         X = 0
         for command in operations:
             if command == "X++" or command == "++X": X += 1
-            if command == "X--" or command == "--X": X -= 1
+            else: X -= 1
         
         return X
