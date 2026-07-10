cac54ed7daea528e4b0b9d49f35db2de6c2c34a4
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-10 10:18:28 -0300

    Time: 0 ms (100%), Space: 12.8 MB (94.58%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -4,5 +4,4 @@ class Solution(object):
         if nums[0] > target: return 0
 
         for num in nums:
-            if num >= target:
-                return nums.index(num)
+            if num >= target: return nums.index(num)
