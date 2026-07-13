8f79ea55db1744d0862a5ebd1bc6b0d3472dd930
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-13 00:54:39 -0300

    Time: 0 ms (100%), Space: 12.4 MB (97.2%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,25 +1,8 @@
 class Solution(object):
     def reverseStr(self, s, k):
-        if len(s) <= k: return s[::-1]
-
-        ans, temp_ans = "", ""
-        first_k_reversed = False
-        n_vezes = 0
-
-        for i in range(len(s)):
-            temp_ans += s[i]
-            if first_k_reversed == False:
-                if i == (k - 1) + (n_vezes * k * 2):
-                    temp_ans = temp_ans[::-1]
-                if i == (k - 1) * 2 + (n_vezes * k * 2):
-                    first_k_reversed = True
-
-            else:
-                ans += temp_ans
-                temp_ans = ""
-                n_vezes += 1
-                first_k_reversed = False
-
-        if len(temp_ans) < k: temp_ans = temp_ans[::-1]
-
-        return ans + temp_ans
+        lista_s = list(s)
+        
+        for i in range(0, len(s), 2 * k):
+            lista_s[i : i + k] = lista_s[i : i + k][::-1]
+            
+        return "".join(lista_s)
