448e12c4afdbdf12d2063b642714329633b3984f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-15 19:49:08 -0300

    Time: 23 ms (5.16%), Space: 12.4 MB (17.12%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -4,7 +4,7 @@ class Solution(object):
         while True:
             new_num = list(str(n))
             n = sum([(int(item) ** 2) for item in new_num])
-            seen.append(int("".join(new_num)))
 
             if n == 1: return True
             elif n in seen: return False
+            seen.append(n)
