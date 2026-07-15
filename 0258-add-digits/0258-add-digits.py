fd1e1db3ed060d1791e8a7a1436257b7dcba3d17
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-15 19:34:25 -0300

    Time: 0 ms (100%), Space: 12.6 MB (0.27%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addDigits(self, num):
+        if len(str(num)) == 1: return num
+
+        while len(str(num)) != 1:
+            new_num = list(str(num))
+            num = sum([int(item) for item in new_num])
+            
+        return num
