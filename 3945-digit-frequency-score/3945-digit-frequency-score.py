c927e01e734733507d6a0219a4e308b178c74d6c
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-16 20:21:59 -0300

    Time: 0 ms (100%), Space: 12.3 MB (52.15%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.py
+++ 3945-digit-frequency-score/3945-digit-frequency-score.py
@@ -1,5 +1,3 @@
 class Solution(object):
     def digitFrequencyScore(self, n):
-        list_n = list(str(n))
-
-        return sum(int(num) for num in list_n)
+        return sum(int(num) for num in list(str(n)))
