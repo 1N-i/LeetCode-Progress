c8e509d1bc3bece713c67a12899a7854f1dfcc1b
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-17 19:23:04 -0300

    Time: 4 ms (18.62%), Space: 12.3 MB (90.57%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def commonFactors(self, a, b):
+        maxAB = max(a, b)
         ans = 0
         
-        for fact in range(1, max(a, b) + 1):
+        for fact in range(1, maxAB + 1):
             if a % fact == 0 and b % fact == 0:
                 ans += 1
 
