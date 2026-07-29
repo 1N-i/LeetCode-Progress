6a9f4a97a55eb4b303abdde9363626d24f4d1362
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-29 20:36:03 -0300

    Time: 21 ms (59.69%), Space: 25.8 MB (86.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,3 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                return True
-        return False
+        return len(set(nums)) != len(nums)
