241232c89e6c781acd16a7a86271670f32568dfc
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-07-29 20:41:17 -0300

    Time: 27 ms (22.98%), Space: 13.6 MB (18.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,3 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        list = []
-        for letter in s:
-            list.append(letter)
-        list.sort()
-        lists = list
-
-        list = []
-        for letter in t:
-            list.append(letter)
-        list.sort()
-        listt = list
-
-        return lists == listt
+        return sorted(s) == sorted(t)
