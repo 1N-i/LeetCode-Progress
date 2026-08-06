9e03ca22668ba1fb973b6b6b0c7df421c4c9aeec
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-05 21:03:52 -0300

    Time: 23 ms (0.48%), Space: 12.4 MB (17.08%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def plusOne(self, digits):
+        len_digits = len(digits)
+        first_nine = False
+
+        for i in range(len_digits - 1, -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                if first_nine == True: digits.pop(i + 1)
+                return digits
+        
+            else:
+                print(digits)
+                if first_nine == False:
+                    digits.pop(i)
+                    digits.extend([1, 0])
+                    first_nine = True
+                
+                else: 
+                    if digits[i - 1] == 9 or digits[0 == 9]:
+                        digits.pop(i)
+                        if len(digits) >= 1: digits.pop(i)
+                        digits.insert(i, 1)
+                        digits.insert(i + 1, 0)
+
+        return digits
