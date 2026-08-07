a31c323d0c246dc7b73354fc47d0532aae5b4062
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-07 12:22:59 -0300

    Time: 61 ms (69.22%), Space: 12.5 MB (35.63%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def nthUglyNumber(self, n):
+        ans = [1]
+        p2 = p3 = p5 = 0
+
+        while len(ans) < n:
+            opt2 = ans[p2] * 2
+            opt3 = ans[p3] * 3
+            opt5 = ans[p5] * 5
+
+            to_add = min(opt2, opt3, opt5)
+            ans.append(to_add)
+
+            if to_add == opt2: p2 += 1
+            if to_add == opt3: p3 += 1
+            if to_add == opt5: p5 += 1
+
+        return ans[-1]
