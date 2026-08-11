9a4a72190f4545f70a172c457ee4bf85f469868f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-11 11:57:35 -0300

    Time: 23 ms (23.73%), Space: 12.4 MB (84.73%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def selfDividingNumbers(self, left, right):
-        ans = []
-
-        for num in range(left, right + 1):
-            if self.doesSelfDivide(num): ans.append(num)
+        ans = [num for num in range(left, right + 1) if self.doesSelfDivide(num)]
 
         return ans
 
     def doesSelfDivide(self, n):
         n_str = str(n)
         for num in n_str:
-            if "0" in num: return False
+            if num == "0": return False
             if n % int(num) != 0: return False
         
         return True
