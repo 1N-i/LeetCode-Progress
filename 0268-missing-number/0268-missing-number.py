299c52dc427739e22b929772a86cb2b4920121ed
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-14 12:10:23 -0300

    Time: 2538 ms (5.04%), Space: 13.4 MB (36.14%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def missingNumber(self, nums):
-        min_num, max_num = min(nums), max(nums)
+        max_num = max(nums)
         for num in range(0, max_num):
             if num not in nums: return num
 
