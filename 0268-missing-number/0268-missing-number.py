e8fecbb939947a931d57952392803d710e778017
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-18 20:08:43 -0300

    Time: 11 ms (34.88%), Space: 13.4 MB (36.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,9 +2,8 @@ class Solution(object):
     def missingNumber(self, nums):
         if 0 not in nums: return 0
 
-        supposed_sum = sum(range(min(nums), max(nums) + 1))
-        actual_sum = sum(nums)
-        if supposed_sum == actual_sum:
+        supposed_list = range(min(nums), max(nums) + 1)
+        if sum(supposed_list) == sum(nums):
             return max(nums) + 1 
         
-        return supposed_sum - actual_sum
+        return sum(supposed_list) - sum(nums)
