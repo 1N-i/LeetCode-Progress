7d794d19383a1965f8830e18652cb4d78d8bc672
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-19 14:55:11 -0300

    Time: 3 ms (66.3%), Space: 13.6 MB (31.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def singleNumber(self, nums):
         supposed_sum = sum(set(nums)) * 3
-        return (supposed_sum - sum(nums)) / 2
+        return (supposed_sum - sum(nums)) // 2
