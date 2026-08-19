1fdb66b6793609490ef416ae08eee9543cece75d
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-19 14:30:53 -0300

    Time: 0 ms (100%), Space: 14 MB (52.94%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-        map_nums = {}
-
+        ans = 0
         for num in nums:
-            map_nums[num] = map_nums.get(num, 0) + 1
+            ans = ans ^ num
 
-        ans = [key for key, qtt in map_nums.items() if qtt == 1]
-        return ans[0]
+        return ans
