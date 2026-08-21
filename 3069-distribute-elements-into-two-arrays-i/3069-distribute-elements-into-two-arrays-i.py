5f31ca20c0142b89c28e25410b2fc9badc79cd9f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-21 09:58:47 -0300

    Time: 0 ms (100%), Space: 12.5 MB (0%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.py
@@ -2,8 +2,13 @@ class Solution(object):
     def resultArray(self, nums):
         arr1, arr2 = [nums[0]], [nums[1]]
 
+        last_added1, last_added2 = nums[0], nums[1]
         for i in range(2, len(nums)):
-            if arr1[-1] > arr2[-1]: arr1.append(nums[i])
-            else: arr2.append(nums[i])
+            if last_added1 > last_added2:
+                arr1.append(nums[i])
+                last_added1 = nums[i]
+            else:
+                arr2.append(nums[i])
+                last_added2 = nums[i]
 
         return arr1 + arr2
