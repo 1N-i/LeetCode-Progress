2df868661851fb1f06e4ba443bb4ab3cd2233d25
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-28 20:18:37 -0300

    Time: 14 ms (78.66%), Space: 12.3 MB (54.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,7 +3,7 @@ class Solution(object):
         if n < 2: return n
         before, last = 0, 1
 
-        for i in range(n - 1):
+        for i in range(n - 1): #Just updates the variables
             before, last = last, before + last
 
         return last
