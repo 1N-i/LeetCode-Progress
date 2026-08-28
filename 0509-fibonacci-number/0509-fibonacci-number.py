006a04754c0fd9953007667e85edfc629ccd936c
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-28 11:21:29 -0300

    Time: 14 ms (78.56%), Space: 12.4 MB (54.47%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,9 @@
 class Solution(object):
     def fib(self, n):
         if n < 2: return n
-
-        list_f = [0, 1]
-
         before, last = 0, 1
 
         for i in range(n - 1):
-            list_f.append(before + last)
             before, last = last, before + last
 
-        return list_f[-1]     
+        return last
