bdc851f889015ce3501c28951e10c9a5545e65f6
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-08-29 10:52:27 -0300

    Time: 2803 ms (5.38%), Space: 65.5 MB (5.38%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -1,16 +1,7 @@
 class Solution(object):
     def triangularSum(self, nums):
-        ans = 0
-        mult = self.getRow(len(nums) - 1)
-        
-        for i in range(len(nums)):
-            ans += nums[i] * mult[i]
-
-        return ans % 10
-
-    def getRow(self, rowIndex):
         triangle = []
-        for row_size in range(rowIndex + 1):
+        for row_size in range(len(nums)):
             triangle_row = [0] * (row_size + 1)
 
             if row_size > 1:
@@ -20,4 +11,10 @@ class Solution(object):
             triangle_row[0], triangle_row[-1] = 1, 1
             triangle.append(triangle_row)
 
-        return triangle[-1]
+        ans = 0
+        mult = triangle[-1]
+        
+        for i in range(len(nums)):
+            ans += nums[i] * mult[i]
+
+        return ans % 10
