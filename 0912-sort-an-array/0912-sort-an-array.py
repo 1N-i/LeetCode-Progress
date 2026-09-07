78a036a9ebf7a3c23e6d89038a510c29a09147d3
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-06 22:40:18 -0300

    Time: 1011 ms (72.62%), Space: 21.3 MB (60.57%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,29 +1,24 @@
 class Solution(object):
     def sortArray(self, nums):
-        if len(nums) <= 1: return nums
+        if len(nums) <= 1: 
+            return nums
 
         mid = len(nums) // 2
-        list_l, list_r = self.sortArray(nums[:mid]), self.sortArray(nums[mid:])
+        list_l = self.sortArray(nums[:mid])
+        list_r = self.sortArray(nums[mid:])
+        
         ans = []
-
-        left, right = 0, 0
-        while left <= len(list_l) or right <= len(list_r):
-            if left >= len(list_l) and right >= len(list_r):
-                return ans
-
-            if left >= len(list_l):
-                ans.append(list_r[right])
-                right += 1
-                continue
-
-            if right >= len(list_r):
-                ans.append(list_l[left])
-                left += 1
-                continue
-
+        left = right = 0
+        
+        while left < len(list_l) and right < len(list_r):
             if list_l[left] < list_r[right]:
                 ans.append(list_l[left])
                 left += 1
             else:
                 ans.append(list_r[right])
                 right += 1
+        
+        ans.extend(list_l[left:])
+        ans.extend(list_r[right:])
+        
+        return ans
