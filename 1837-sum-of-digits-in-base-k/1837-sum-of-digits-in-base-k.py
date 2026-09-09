dce13ab6b749c7a210608cf21e35dee732494490
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-09 12:45:26 -0300

    Time: 0 ms (100%), Space: 12.3 MB (49.39%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def sumBase(self, n, k):
-        import string
-
         convertedNum = ""
-        chars = string.digits + string.ascii_uppercase
         while n != 0:
-            remain = n % k
-            convertedNum += chars[remain]
+            convertedNum += str(n % k)
             n //= k
 
         ans = 0
