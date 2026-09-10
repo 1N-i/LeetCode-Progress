d4a9546a61fd6d34f9070a1b9e0cb032317ed000
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-09 23:10:52 -0300

    Time: 15 ms (39.51%), Space: 12.4 MB (19.9%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -3,7 +3,7 @@ class Solution(object):
         if n == 0: return False
         if n == 1: return True
         while n % 3 == 0:
-            n /= 3
+            n //= 3
             if n == 1: return True
 
         return False
