05a24d6d62c3b9c6ad831541c2ddeb540c2cf6b9
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-12 01:20:02 -0300

    Time: 16 ms (69.45%), Space: 12.4 MB (52.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -3,7 +3,7 @@ class Solution(object):
         plus_minus = 1 if x > 0 else -1
 
         str_x = str(x * plus_minus)
-        ans = "".join(num for num in str_x[::-1])
+        ans = "".join(num for num in reversed(str_x))
 
         int_ans = int(ans)
 
