6c521711982754476a21162b32929ae5fc3e804b
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-15 20:51:35 -0300

    Time: 4 ms (15.98%), Space: 12.3 MB (91.79%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        nums.sort()
+
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] == target:
+                ans.append(i)
+
+                for j in range(i + 1, len(nums)):
+                    if nums[j] != target:
+                        break
+                    ans.append(j)
+
+                break
+
+        return ans
