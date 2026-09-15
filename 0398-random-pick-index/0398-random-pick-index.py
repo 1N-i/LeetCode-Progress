521a30788a8457d2a4e84c40407a5cf5bcc33420
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-15 20:24:52 -0300

    Time: 72 ms (45.53%), Space: 23.1 MB (85.37%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.py
@@ -0,0 +1,13 @@
+import random
+class Solution(object):
+    def __init__(self, nums):
+        self.map = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num not in self.map:
+                self.map[num] = []
+
+            self.map[num].append(i)
+
+    def pick(self, target):
+        return random.choice(self.map[target])
