4f7f652fab4831043b27658ee35c38673f224485
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-18 11:57:10 -0300

    Time: 3 ms (51.41%), Space: 12.4 MB (90.76%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -4,10 +4,12 @@ class Solution(object):
         not_arr2 = []
 
         for num in arr1:
-            map_ans[num] = map_ans.get(num, 0) + 1
-
+            if num not in map_ans:
+                map_ans[num] = 0
             if num not in arr2:
                 not_arr2.append(num)
+
+            map_ans[num] += 1
 
         for num in arr2:
             ans.extend([num] * map_ans[num])
