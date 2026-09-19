2dabe5c19c170199c3d8fec8ab77ae69dbfde90f
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-19 17:01:59 -0300

    Time: 28 ms (65.56%), Space: 13.1 MB (19.15%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def maxDistinct(self, s):
-        guide = {}
-
-        for l in s:
-            guide[l] = guide.get(l, 0) + 1
-
-        return len(guide)
+        return len(set(s))
