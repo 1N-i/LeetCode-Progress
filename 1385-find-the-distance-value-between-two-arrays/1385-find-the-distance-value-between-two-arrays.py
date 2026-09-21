8425c326d6574a5c6fa0e317dae97d79675442e7
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-21 00:58:46 -0300

    Time: 23 ms (55.02%), Space: 12.5 MB (16.06%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -2,7 +2,6 @@ class Solution(object):
     def findTheDistanceValue(self, arr1, arr2, d):
         ans = len(arr1)
         for num1 in arr1:
-            valid = True
             for num2 in arr2:
                 if abs(num1 - num2) <= d:
                     ans -= 1
