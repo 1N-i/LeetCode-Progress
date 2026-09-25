33bb93ceb94757f189f4cfcb30b2cf78339d7be3
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-09-25 20:49:43 -0300

    Time: 8033 ms (5.02%), Space: 12.7 MB (23.27%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         ransom_map, magazine_map = {}, {}
-
         for l in ransomNote:
-            ransom_map[l] = ransom_map.get(l, 0) + 1
+            ransom_map[l] = ransomNote.count(l)
         for l in magazine:
-            magazine_map[l] = magazine_map.get(l, 0) + 1
+            magazine_map[l] = magazine.count(l)
 
         for l in ransom_map:
             if magazine_map.get(l, 0) == 0 or ransom_map[l] > magazine_map[l]:
